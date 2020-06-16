0bc02cc007c595a268b04dfac4262223fa9e613c
Nikita <nikput05@gmail.com>
2020-06-16 14:02:22 +0300

    Доработка кода, разбиение его на функции

--- js/main.js
+++ js/main.js
@@ -54,15 +54,15 @@ var createNewPin = function (templateElement, offerInfo) {
   return element;
 };
 
-var showOffers = function (map) {
+var showOffers = function () {
   var mapPins = map.querySelector('.map__pins');
   var similarOffers = createSimilarOffers(mapPins.offsetWidth);
 
   var pin = document.querySelector('#pin').content.querySelector('.map__pin');
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < similarOffers.length; i++) {
-    fragment.appendChild(createNewPin(pin, similarOffers[i]));
-  }
+  similarOffers.forEach(function (offer) {
+    fragment.appendChild(createNewPin(pin, offer));
+  });
   mapPins.appendChild(fragment);
 };
 
@@ -112,142 +112,154 @@ var showOfferCard = function () {
 };
 */
 
-var checkTitle = function (title) {
-  if (title.validity.tooShort) {
-    title.setCustomValidity('Заголовок должен быть от 30-ти символов, а у вас их ' + title.value.length);
-  } else if (title.validity.tooLong) {
-    title.setCustomValidity('Заголовок должен быть до 100 символов, а у вас их ' + title.value.length);
-  } else if (title.validity.valueMissing) {
-    title.setCustomValidity('Обязательное поле');
+var checkTitle = function () {
+  if (adFormTitle.validity.tooShort) {
+    adFormTitle.setCustomValidity('Заголовок должен быть от 30-ти символов, а у вас их ' + adFormTitle.value.length);
+  } else if (adFormTitle.validity.tooLong) {
+    adFormTitle.setCustomValidity('Заголовок должен быть до 100 символов, а у вас их ' + adFormTitle.value.length);
+  } else if (adFormTitle.validity.valueMissing) {
+    adFormTitle.setCustomValidity('Обязательное поле');
   } else {
-    title.setCustomValidity('');
+    adFormTitle.setCustomValidity('');
   }
 };
 
-var setHousePrice = function (typeOfHouse, price) {
-  if (typeOfHouse.value === 'bungalo') {
-    price.min = '0';
-    price.placeholder = '0';
-  } else if (typeOfHouse.value === 'flat') {
-    price.min = '1000';
-    price.placeholder = '1000';
-  } else if (typeOfHouse.value === 'house') {
-    price.min = '5000';
-    price.placeholder = '5000';
+var setHousePrice = function () {
+  if (adFormTypeOfHouse.value === 'bungalo') {
+    adFormPrice.min = '0';
+    adFormPrice.placeholder = '0';
+  } else if (adFormTypeOfHouse.value === 'flat') {
+    adFormPrice.min = '1000';
+    adFormPrice.placeholder = '1000';
+  } else if (adFormTypeOfHouse.value === 'house') {
+    adFormPrice.min = '5000';
+    adFormPrice.placeholder = '5000';
   } else {
-    price.min = '10000';
-    price.placeholder = '10000';
+    adFormPrice.min = '10000';
+    adFormPrice.placeholder = '10000';
   }
 };
 
-var checkHousePrice = function (typeOfHouse, price) {
-  if (price.validity.rangeUnderflow) {
-    price.setCustomValidity('Цена за ночь при типе жилья \"' + typeOfHouse.options[typeOfHouse.selectedIndex].textContent + '\" должна быть не меньше ' + price.min);
-  } else if (price.validity.rangeOverflow) {
-    price.setCustomValidity('Цена за ночь при типе жилья \"' + typeOfHouse.options[typeOfHouse.selectedIndex].textContent + '\" должна быть не больше ' + price.max);
-  } else if (price.validity.valueMissing) {
-    price.setCustomValidity('Обязательное поле');
+var checkHousePrice = function () {
+  if (adFormPrice.validity.rangeUnderflow) {
+    adFormPrice.setCustomValidity('Цена за ночь при типе жилья \"' + adFormTypeOfHouse.options[adFormTypeOfHouse.selectedIndex].textContent + '\" должна быть не меньше ' + adFormPrice.min);
+  } else if (adFormPrice.validity.rangeOverflow) {
+    adFormPrice.setCustomValidity('Цена за ночь при типе жилья \"' + adFormTypeOfHouse.options[adFormTypeOfHouse.selectedIndex].textContent + '\" должна быть не больше ' + adFormPrice.max);
+  } else if (adFormPrice.validity.valueMissing) {
+    adFormPrice.setCustomValidity('Обязательное поле');
   } else {
-    price.setCustomValidity('');
+    adFormPrice.setCustomValidity('');
   }
 };
 
-var checkRoomNumberAndCapacity = function (roomNumber, capacity) {
-  if (roomNumber.value === '1' && !(capacity.value === '1')) {
-    capacity.setCustomValidity('В 1-й комнате может быть только 1 гость');
-  } else if (roomNumber.value === '2' && !(capacity.value === '1' || capacity.value === '2')) {
-    capacity.setCustomValidity('В 2-х комнатах может быть 1 или 2 гостя');
-  } else if (roomNumber.value === '3' && !(capacity.value === '1' || capacity.value === '2' || capacity.value === '3')) {
-    capacity.setCustomValidity('В 3-х комнатах может быть от 1 до 3 гостей');
-  } else if (roomNumber.value === '100' && !(capacity.value === '0')) {
-    capacity.setCustomValidity('100 комнат – не для гостей');
+var checkRoomNumberAndCapacity = function () {
+  if (adFormRoomNumber.value === '1' && !(adFormCapacity.value === '1')) {
+    adFormCapacity.setCustomValidity('В 1-й комнате может быть только 1 гость');
+  } else if (adFormRoomNumber.value === '2' && !(adFormCapacity.value === '1' || adFormCapacity.value === '2')) {
+    adFormCapacity.setCustomValidity('В 2-х комнатах может быть 1 или 2 гостя');
+  } else if (adFormRoomNumber.value === '3' && !(adFormCapacity.value === '1' || adFormCapacity.value === '2' || adFormCapacity.value === '3')) {
+    adFormCapacity.setCustomValidity('В 3-х комнатах может быть от 1 до 3 гостей');
+  } else if (adFormRoomNumber.value === '100' && !(adFormCapacity.value === '0')) {
+    adFormCapacity.setCustomValidity('100 комнат – не для гостей');
   } else {
-    capacity.setCustomValidity('');
+    adFormCapacity.setCustomValidity('');
   }
 };
 
-var activateSite = function (map, form, formControls) {
+var writeAdFormAddress = function () {
+  adFormAdress.value = (mainPin.offsetLeft - MAIN_PIN_WIDTH / 2) + ', ' + (mainPin.offsetTop - MAIN_PIN_HEIGHT);
+};
+
+var disableSite = function () {
+  adFormControls.forEach(function (control) {
+    control.disabled = true;
+  });
+};
+
+var activateSite = function () {
   map.classList.remove('map--faded');
-  form.classList.remove('ad-form--disabled');
+  adForm.classList.remove('ad-form--disabled');
 
-  for (var i = 0; i < formControls.length; i++) {
-    formControls[i].disabled = false;
-  }
+  adFormControls.forEach(function (control) {
+    control.disabled = false;
+  });
 
-  var formAdress = form.querySelector('#address');
-  formAdress.readOnly = true;
+  adFormAdress.readOnly = true;
 
-  showOffers(map);
+  showOffers();
 };
+
+var addEventListenersOnFormElements = function () {
+  adFormTitle.addEventListener('invalid', function () {
+    checkTitle();
+  });
+  adFormTitle.addEventListener('input', function () {
+    checkTitle();
+  });
+
+
+  adFormTypeOfHouse.addEventListener('change', function () {
+    setHousePrice();
+  });
+  adFormPrice.addEventListener('invalid', function () {
+    checkHousePrice();
+  });
+  adFormPrice.addEventListener('input', function () {
+    checkHousePrice();
+  });
+
+
+  adFormTimeIn.addEventListener('change', function () {
+    adFormTimeOut.value = adFormTimeIn.value;
+  });
+  adFormTimeOut.addEventListener('change', function () {
+    adFormTimeIn.value = adFormTimeOut.value;
+  });
+
+
+  adFormRoomNumber.addEventListener('change', function () {
+    checkRoomNumberAndCapacity();
+  });
+  adFormCapacity.addEventListener('change', function () {
+    checkRoomNumberAndCapacity();
+  });
+};
+
+var onMainPinPress = function () {
+  activateSite();
+  writeAdFormAddress();
+  addEventListenersOnFormElements();
+};
+
 
 var map = document.querySelector('.map');
 var mainPin = map.querySelector('.map__pin--main');
+
 var adForm = document.querySelector('.ad-form');
-var adFormControls = adForm.querySelectorAll('input, select');
-for (var i = 0; i < adFormControls.length; i++) {
-  adFormControls[i].disabled = true;
-}
-
-
+var adFormControls = adForm.querySelectorAll('input, select, button, textarea');
 var adFormAdress = adForm.querySelector('#address');
-adFormAdress.value = (mainPin.offsetLeft - MAIN_PIN_WIDTH / 2) + ', ' + (mainPin.offsetTop - MAIN_PIN_HEIGHT);
+var adFormTitle = adForm.querySelector('#title');
+var adFormTypeOfHouse = adForm.querySelector('#type');
+var adFormPrice = adForm.querySelector('#price');
+var adFormTimeIn = adForm.querySelector('#timein');
+var adFormTimeOut = adForm.querySelector('#timeout');
+var adFormRoomNumber = adForm.querySelector('#room_number');
+var adFormCapacity = adForm.querySelector('#capacity');
+
+disableSite();
+writeAdFormAddress();
+setHousePrice();
+checkRoomNumberAndCapacity();
+
 mainPin.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
   if (evt.button === 0) {
-    activateSite(map, adForm, adFormControls);
-    adFormAdress.value = (mainPin.offsetLeft - MAIN_PIN_WIDTH / 2) + ', ' + (mainPin.offsetTop - MAIN_PIN_HEIGHT);
+    onMainPinPress();
   }
 });
 mainPin.addEventListener('keydown', function (evt) {
   evt.preventDefault();
   if (evt.key === 'Enter') {
-    activateSite(map, adForm, adFormControls);
+    onMainPinPress();
   }
-});
-
-
-var adFormTitle = adForm.querySelector('#title');
-adFormTitle.addEventListener('invalid', function () {
-  checkTitle(adFormTitle);
-});
-adFormTitle.addEventListener('input', function () {
-  checkTitle(adFormTitle);
-});
-
-
-var adFormTypeOfHouse = adForm.querySelector('#type');
-var adFormPrice = adForm.querySelector('#price');
-setHousePrice(adFormTypeOfHouse, adFormPrice);
-
-adFormTypeOfHouse.addEventListener('change', function () {
-  setHousePrice(adFormTypeOfHouse, adFormPrice);
-});
-adFormPrice.addEventListener('invalid', function () {
-  checkHousePrice(adFormTypeOfHouse, adFormPrice);
-});
-adFormPrice.addEventListener('input', function () {
-  checkHousePrice(adFormTypeOfHouse, adFormPrice);
-});
-
-
-var adFormTimeIn = adForm.querySelector('#timein');
-var adFormTimeOut = adForm.querySelector('#timeout');
-adFormTimeIn.addEventListener('change', function () {
-  adFormTimeOut.value = adFormTimeIn.value;
-});
-adFormTimeOut.addEventListener('change', function () {
-  adFormTimeIn.value = adFormTimeOut.value;
-});
-
-
-var adFormRoomNumber = adForm.querySelector('#room_number');
-var adFormCapacity = adForm.querySelector('#capacity');
-
-checkRoomNumberAndCapacity(adFormRoomNumber, adFormCapacity);
-
-adFormRoomNumber.addEventListener('change', function () {
-  checkRoomNumberAndCapacity(adFormRoomNumber, adFormCapacity);
-});
-adFormCapacity.addEventListener('change', function () {
-  checkRoomNumberAndCapacity(adFormRoomNumber, adFormCapacity);
 });
