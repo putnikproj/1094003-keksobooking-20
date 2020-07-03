9d4be137ca36fb9f3e7d74069393bebe5dd829e5
Nikita <nikput05@gmail.com>
2020-07-03 13:19:04 +0300

    Доделана фильтрация объявлений

---
+++ js/debounce.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var DEBOUNCE_INTERVAL = 500;
+
+  window.debounce = function (cb) {
+    var lastTimeout = null;
+
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+})();
